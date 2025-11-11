f2eb1e65a4426434a9b28cb2c24f31f951362024
Hasan Ali <hans1998ali@gmail.com>
2025-11-11 13:05:32 +0530

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+      const { name, value } = e.target;
+
+      if (name === 'email') {
+         setEmail(value);
+      } else {
+         setPassword(value);
+      }
+   }
+
+   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+      e.preventDefault();
+      console.log({ email, password });
+      alert('Login successful');
+   }
+
+   return (
+      <div className='login_page'>
+         <form onSubmit={handleSubmit}>
+            <input
+               type='text'
+               name='email'
+               value={email}
+               onChange={handleChange}
+            />
+            <input
+               type='password'
+               name='password'
+               value={password}
+               onChange={handleChange}
+            />
+            <button>Login</button>
+         </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   );
 }
 
-export default App
+export default App;
